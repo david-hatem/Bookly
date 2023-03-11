e0ae15ab4a448af9aef4cdb00b8b884818926c97
David Hatem <davidhatem880@gmail.com>
2023-03-11 16:43:15 +0200

    add featured list view to home

--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -1,4 +1,5 @@
 class AssetsData {
   static const logo = 'assets/images/Logo.png';
-  static const testImage = 'assets/images/test_image.png';
+  // static const testImage = 'assets/images/test_image.png';
+  static const testImage = 'assets/images/61m5AlecPTL.jpg';
 }
